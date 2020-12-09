d34fe4fb1e7f8e64a67cb5f16320ebdc0d873459
jamesrplee <james.rp.lee@gmail.com>
2020-12-09 15:46:08 +1000

    debugging the sending of SVG

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import './App.css';
 function App(svg) {
   return (
     <div className="App">
-      <div
-        id="svg"
-      >
-        <svg
-          width={svg && svg.width.baseVal.value}
-          height={svg && svg.height.baseVal.value}
-          viewBox={svg && `${svg.viewBox.baseVal.x} ${svg.viewBox.baseVal.y} ${svg.viewBox.baseVal.width} ${svg.viewBox.baseVal.height}`}
-          dangerouslySetInnerHTML={svg && { __html: svg.innerHTML }}
-        />
-      </div>
+      {svg && svg.innerHTML}
     </div>
   );
 }
