e01e485f86947b0d716f937710457e347da36049
jamesrplee <james.rp.lee@gmail.com>
2020-12-09 15:40:34 +1000

    removing generative component and adding basic SVG handling

--- src/App.js
+++ src/App.js
@@ -1,141 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import C2S from 'canvas2svg'
 
 
-function App() {
+function App(svg) {
   return (
     <div className="App">
-      <MondrianCanvas />
-    </div>
-  );
-}
-
-const MondrianCanvas = props => {
-  const [svg, setSvg] = useState(null)
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    var context = new C2S(window.innerWidth, window.innerWidth)
-
-    const size = window.innerWidth;
-    const dpr = window.devicePixelRatio;
-
-    canvas.width = size * dpr;
-    canvas.height = size * dpr;
-    context.lineWidth = 4;
-
-    const step = size / 7;
-    const white = '#F2F5F1';
-    const colors = ['#D40920', '#1356A2', '#F7D842']
-
-    const squares = [{
-        x: 0,
-        y: 0,
-        width: size,
-        height: size
-      }];
-
-    function splitSquaresWith(coordinates) {
-      const { x, y } = coordinates;
-
-      for (var i = squares.length - 1; i >= 0; i--) {
-      const square = squares[i];
-
-      if (x && x > square.x && x < square.x + square.width) {
-          if(Math.random() > 0.5) {
-            squares.splice(i, 1);
-            splitOnX(square, x);
-          }
-      }
-
-      if (y && y > square.y && y < square.y + square.height) {
-          if(Math.random() > 0.5) {
-            squares.splice(i, 1);
-            splitOnY(square, y);
-          }
-      }
-      }
-    }
-
-    function splitOnX(square, splitAt) {
-      var squareA = {
-        x: square.x,
-        y: square.y,
-        width: square.width - (square.width - splitAt + square.x),
-        height: square.height
-      };
-
-      var squareB = {
-      x: splitAt,
-      y: square.y,
-      width: square.width - splitAt + square.x,
-      height: square.height
-      };
-
-      squares.push(squareA);
-      squares.push(squareB);
-    }
-
-    function splitOnY(square, splitAt) {
-      var squareA = {
-        x: square.x,
-        y: square.y,
-        width: square.width,
-        height: square.height - (square.height - splitAt + square.y)
-      };
-
-      var squareB = {
-      x: square.x,
-      y: splitAt,
-      width: square.width,
-      height: square.height - splitAt + square.y
-      };
-
-      squares.push(squareA);
-      squares.push(squareB);
-    }
-
-    for (var i = 0; i < size; i += step) {
-      splitSquaresWith({ y: i });
-      splitSquaresWith({ x: i });
-    }
-
-    function draw() {
-      for (var i = 0; i < colors.length; i++) {
-        squares[Math.floor(Math.random() * squares.length)].color = colors[i];
-      }
-      for (var i = 0; i < squares.length; i++) {
-        context.beginPath();
-        context.rect(
-          squares[i].x,
-          squares[i].y,
-          squares[i].width,
-          squares[i].height
-        );
-        if(squares[i].color) {
-          context.fillStyle = squares[i].color;
-        } else {
-          context.fillStyle = white
-        }
-        context.fill()
-        context.stroke();
-      }
-    }
-
-    draw()
-
-    var mySvg = context.getSvg()
-    mySvg.setAttribute("viewBox", `0 0 ${window.innerWidth} ${window.innerWidth}`)
-    setSvg(mySvg)
-
-  }, []) // useEffect
-
-  return (
-    <>
-      <canvas ref={canvasRef} {...props} />
-
       <div
         id="svg"
       >
@@ -146,8 +15,8 @@ const MondrianCanvas = props => {
           dangerouslySetInnerHTML={svg && { __html: svg.innerHTML }}
         />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App;
